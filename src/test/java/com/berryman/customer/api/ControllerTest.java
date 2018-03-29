88563fdf963a2b1cacea40532f9f59ccd6c4cdb7
cberryman <cberryman@ebay.com>
2018-03-29 11:54:07 +0100

    amended dependencies, configured swagger

--- src/test/java/com/berryman/customer/api/ControllerTest.java
+++ src/test/java/com/berryman/customer/api/ControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
